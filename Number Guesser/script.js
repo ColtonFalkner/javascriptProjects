7d3ac6ca9739a99905a2e8a63de083f84286ab7d
Colton <coltonfalkner@gmail.com>
2021-03-17 16:51:00 -0400

    reset function without changing state

--- Number Guesser/script.js
+++ Number Guesser/script.js
@@ -1,7 +1,6 @@
 'use strict'
-const secretNumber = Math.trunc(Math.random() * 20) + 1
+let secretNumber = Math.trunc(Math.random() * 20) + 1
 let score = 20
-document.querySelector('.number').textContent = secretNumber
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value)
@@ -9,14 +8,44 @@ document.querySelector('.check').addEventListener('click', function () {
   if (!guess) {
     document.querySelector('.message').textContent = '⛔️ No Number'
   } else if (guess === secretNumber) {
+    //When Player Wins
     document.querySelector('.message').textContent = '🎉 Correct Number!!'
+    document.querySelector('.number').textContent = secretNumber
+
+    document.querySelector('body').style.backgroundColor = '#60b447'
+    document.querySelector('.number').style.width = '30rem'
   } else if (guess > secretNumber) {
-    document.querySelector('.message').textContent = 'Too High!'
-    score--
-    document.querySelector('.score').textContent = score
+    if (score > 1) {
+      //when guess is too high
+      document.querySelector('.message').textContent = 'Too High!'
+      score--
+      document.querySelector('.score').textContent = score
+    } else {
+      document.querySelector('.message').textContent = '😩 You Lose!'
+      document.querySelector('.score').textContent = 0
+    }
   } else if (guess < secretNumber) {
-    document.querySelector('.message').textContent = 'Too Low!'
-    score--
-    document.querySelector('.score').textContent = score
+    if (score > 1) {
+      //when guess is too low
+      document.querySelector('.message').textContent = 'Too Low!'
+      score--
+      document.querySelector('.score').textContent = score
+    } else {
+      document.querySelector('.message').textContent = '😩 You Lose!'
+      document.querySelector('.score').textContent = 0
+    }
   }
+})
+//Reset The Game
+document.querySelector('.again').addEventListener('click', () => {
+  score = 20
+  secretNumber = Math.trunc(Math.random() * 20) + 1
+
+  document.querySelector('.message').textContent = 'Start guessing...'
+  document.querySelector('.score').textContent = score
+  document.querySelector('.number').textContent = '?'
+  document.querySelector('.guess').value = ''
+
+  document.querySelector('body').style.backgroundColor = '#222'
+  document.querySelector('.number').style.width = '15rem'
 })
