c1bd2f36d76ce997c7841072b18da332ceda4a1e
Colton <coltonfalkner@gmail.com>
2021-03-17 17:14:32 -0400

    refactor successful

--- Number Guesser/script.js
+++ Number Guesser/script.js
@@ -1,6 +1,7 @@
 'use strict'
 let secretNumber = Math.trunc(Math.random() * 20) + 1
 let score = 20
+let highscore = 0
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value)
@@ -14,20 +15,18 @@ document.querySelector('.check').addEventListener('click', function () {
 
     document.querySelector('body').style.backgroundColor = '#60b447'
     document.querySelector('.number').style.width = '30rem'
-  } else if (guess > secretNumber) {
+
+    if (score > highscore) {
+      highscore = score
+      document.querySelector('.highscore').textContent = highscore
+    }
+  }
+  //When guess is different.
+  else if (guess !== secretNumber) {
     if (score > 1) {
       //when guess is too high
-      document.querySelector('.message').textContent = 'Too High!'
-      score--
-      document.querySelector('.score').textContent = score
-    } else {
-      document.querySelector('.message').textContent = '😩 You Lose!'
-      document.querySelector('.score').textContent = 0
-    }
-  } else if (guess < secretNumber) {
-    if (score > 1) {
-      //when guess is too low
-      document.querySelector('.message').textContent = 'Too Low!'
+      document.querySelector('.message').textContent =
+        guess > secretNumber ? 'Too High!' : 'Too Low!'
       score--
       document.querySelector('.score').textContent = score
     } else {
