0dbf117ae7361cc67ac4739fdefdb7ef26c7f579
Colton <coltonfalkner@gmail.com>
2021-03-17 17:26:58 -0400

    further refactoring implemented

--- Number Guesser/script.js
+++ Number Guesser/script.js
@@ -3,14 +3,18 @@ let secretNumber = Math.trunc(Math.random() * 20) + 1
 let score = 20
 let highscore = 0
 
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message
+}
+
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value)
 
   if (!guess) {
-    document.querySelector('.message').textContent = '⛔️ No Number'
+    displayMessage('⛔️ No Number')
   } else if (guess === secretNumber) {
     //When Player Wins
-    document.querySelector('.message').textContent = '🎉 Correct Number!!'
+    displayMessage('🎉 Correct Number!!')
     document.querySelector('.number').textContent = secretNumber
 
     document.querySelector('body').style.backgroundColor = '#60b447'
@@ -25,12 +29,12 @@ document.querySelector('.check').addEventListener('click', function () {
   else if (guess !== secretNumber) {
     if (score > 1) {
       //when guess is too high
-      document.querySelector('.message').textContent =
-        guess > secretNumber ? 'Too High!' : 'Too Low!'
+      displayMessage(guess > secretNumber ? 'Too High!' : 'Too Low!')
+
       score--
       document.querySelector('.score').textContent = score
     } else {
-      document.querySelector('.message').textContent = '😩 You Lose!'
+      displayMessage('😩 You Lose!')
       document.querySelector('.score').textContent = 0
     }
   }
@@ -40,7 +44,7 @@ document.querySelector('.again').addEventListener('click', () => {
   score = 20
   secretNumber = Math.trunc(Math.random() * 20) + 1
 
-  document.querySelector('.message').textContent = 'Start guessing...'
+  displayMessage('Start guessing...')
   document.querySelector('.score').textContent = score
   document.querySelector('.number').textContent = '?'
   document.querySelector('.guess').value = ''
