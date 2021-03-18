e23710af82ddfce70f9c3821d48c946a0c1b2606
Colton <coltonfalkner@gmail.com>
2021-03-18 15:00:07 -0400

    modals open and close on click

---
+++ Modal Window/script.js
@@ -0,0 +1,28 @@
+'use strict'
+
+const modal = document.querySelector('.modal')
+const overlay = document.querySelector('.overlay')
+const btnCloseModal = document.querySelector('.close-modal')
+const btnsShowModal = document.querySelectorAll('.show-modal')
+
+const openModal = () => {
+  modal.classList.remove('hidden')
+  overlay.classList.remove('hidden')
+}
+
+const closeModal = function () {
+  modal.classList.add('hidden')
+  overlay.classList.add('hidden')
+}
+
+for (let i = 0; i < btnsShowModal.length; i++) {
+  btnsShowModal[i].addEventListener('click', openModal)
+}
+
+btnCloseModal.addEventListener('click', closeModal)
+
+overlay.addEventListener('click', closeModal)
+
+document.addEventListener('keydown', () => {
+  console.log('A key was pressed')
+})
