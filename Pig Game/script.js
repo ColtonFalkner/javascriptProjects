6ecc5aed5c18728a627f33e79695f1baf13e2414
Colton <coltonfalkner@gmail.com>
2021-03-19 13:08:20 -0400

    toggle player implemented

--- Pig Game/script.js
+++ Pig Game/script.js
@@ -1,5 +1,7 @@
 'use strict'
 
+const player0El = document.querySelector('.player--0')
+const player1El = document.querySelector('.player--1')
 const score0El = document.getElementById('score--0')
 const score1El = document.getElementById('score--1')
 const current0El = document.getElementById('current--0')
@@ -14,7 +16,7 @@ score0El.textContent = 0
 score1El.textContent = 0
 diceEl.classList.add('hidden')
 
-let score = [0, 0]
+let scores = [0, 0]
 let currentScore = 0
 let activePlayer = 0
 
@@ -31,8 +33,15 @@ btnRoll.addEventListener('click', function () {
   if (dice !== 1) {
     //add dice
     currentScore += dice
-    current0El.textContent = currentScore
+    document.getElementById(
+      `current--${activePlayer}`
+    ).textContent = currentScore
   } else {
     //switch player
+    document.getElementById(`current--${activePlayer}`).textContent = 0
+    currentScore = 0
+    activePlayer = activePlayer === 0 ? 1 : 0
+    player0El.classList.toggle('player--active')
+    player1El.classList.toggle('player--active')
   }
 })
