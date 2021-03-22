f8a5a5ddcd57a5b25a62693ea6ae4108a7f2ccea
Colton <coltonfalkner@gmail.com>
2021-03-22 12:39:47 -0400

    scoping practice complete

--- ScopingInPractice/script.js
+++ ScopingInPractice/script.js
@@ -5,13 +5,24 @@ function calcAge(birthYear) {
   console.log(firstName)
 
   function printAge() {
-    const output = `You are ${age}, born in ${birthYear}.`
+    let output = `You are ${age}, born in ${birthYear}.`
     console.log(output)
 
     if (birthYear >= 1981 && birthYear <= 1996) {
+      var millenial = true
+      const firstName = 'Crampton'
+      output = 'new OUtput'
       const str = `Oh, and you're a millenial, ${firstName}`
       console.log(str)
+
+      function add(a, b) {
+        return a + b
+      }
+
+      console.log(add(2, 4))
     }
+    // console.log(millenial)
+    console.log(output)
   }
   printAge()
 
