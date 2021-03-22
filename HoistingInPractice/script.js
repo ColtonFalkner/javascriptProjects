e9f48119e10e193767f746c8623ac911dd68e29e
Colton <coltonfalkner@gmail.com>
2021-03-22 13:02:23 -0400

    the temporal dead zone should be cooler

--- HoistingInPractice/script.js
+++ HoistingInPractice/script.js
@@ -1,9 +1,32 @@
 'use strict'
 
-console.log(me)
-console.log(job)
-console.log(year)
+// console.log(me)
+// console.log(job)
+// console.log(year)
 
 var me = 'colton'
 let job = 'unemployed'
 const year = 1991
+
+console.log(addDecl(2, 2))
+// console.log(addExp(2, 2))
+// console.log(addArror(2, 2))
+
+//Function Declaration
+function addDecl(a, b) {
+  return a + b
+}
+//Function Expression
+const addExp = function (a, b) {
+  return a + b
+}
+//Arrow function
+const addArror = (a, b) => a + b
+
+if (!numProd) deleteShoppingCart()
+
+var numProd = 10
+
+function deleteShoppingCart() {
+  console.log('All products deleted')
+}
