5f04ae6472d701ce268f2099629397d7a85337a2
Colton <coltonfalkner@gmail.com>
2021-03-22 11:30:47 -0400

    scoping is not a mystery anymore

---
+++ ScopingInPractice/script.js
@@ -0,0 +1,23 @@
+'use strict'
+
+function calcAge(birthYear) {
+  const age = 2037 - birthYear
+  console.log(firstName)
+
+  function printAge() {
+    const output = `You are ${age}, born in ${birthYear}.`
+    console.log(output)
+
+    if (birthYear >= 1981 && birthYear <= 1996) {
+      const str = `Oh, and you're a millenial, ${firstName}`
+      console.log(str)
+    }
+  }
+  printAge()
+
+  return age
+}
+
+const firstName = 'Colton'
+
+calcAge(1991)
