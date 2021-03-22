b8d1a59fb97948d4f4569173f04f9d7d935b2e67
Colton <coltonfalkner@gmail.com>
2021-03-22 13:33:38 -0400

    this makes sense now

--- ThisInPractice/script.js
+++ ThisInPractice/script.js
@@ -8,3 +8,27 @@ const calcAge = function (birthYear) {
 }
 
 calcAge(1991)
+
+const calcAgeArrow = (birthYear) => {
+  console.log(2037 - birthYear)
+  console.log(this)
+}
+
+calcAgeArrow(1991)
+
+const colton = {
+  year: 1991,
+  calcAge: function () {
+    console.log(this)
+    console.log(2037 - this.year)
+  },
+}
+
+colton.calcAge()
+
+const matilda = {
+  year: 2017,
+}
+
+matilda.calcAge = colton.calcAge
+matilda.calcAge()
