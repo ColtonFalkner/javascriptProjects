ad5e54aa727f6e223fea7c0c97ab7f57c982b6c4
Colton <coltonfalkner@gmail.com>
2021-03-23 14:49:23 -0400

    nunderstand so much better now

--- FunctionsInPractice/script.js
+++ FunctionsInPractice/script.js
@@ -4,11 +4,32 @@ const colton = {
   firstName: 'Colton',
   year: 1991,
   calcAge: function () {
-    console.log(this)
-    console.log(2037 - this.year)
+    // console.log(this)
+    // console.log(2037 - this.year)
+
+    const self = this
+    const isMillenial = function () {
+      console.log(self)
+      console.log(self.year >= 1981 && self.year <= 1996)
+    }
+    isMillenial()
   },
 
   greet: () => console.log(`Hey ${this.firstName}`),
 }
 
-console.log(colton.greet())
+colton.calcAge()
+
+const addExpr = function (a, b) {
+  console.log(arguments)
+  return a + b
+}
+addExpr(2, 5)
+addExpr(2, 5, 8, 12)
+
+var addArrow = (a, b) => {
+  console.log(arguments)
+  a + b
+}
+
+addArrow(1, 2)
