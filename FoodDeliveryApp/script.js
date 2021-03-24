a671264514e7ff20f187b7890445d1af2418f268
Colton <coltonfalkner@gmail.com>
2021-03-24 16:16:17 -0400

    slightly better understanding of how to destructure arrays

--- FoodDeliveryApp/script.js
+++ FoodDeliveryApp/script.js
@@ -12,6 +12,10 @@ const restaurant = {
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]]
+  },
+
   openingHours: {
     thu: {
       open: 12,
@@ -35,3 +39,21 @@ const c = arr[2]
 
 const [x, y, z] = arr
 console.log(x, y, z)
+
+// let [main, , secondary] = restaurant.categories
+
+console.log(restaurant.order(2, 0))
+
+const [starter, main] = restaurant.order(2, 0)
+console.log(starter, main)
+
+const nested = [2, 4, [5, 6]]
+// const [i, , j] = nested
+// console.log(i, j)
+//destructuring nested array
+const [i, , [j, k]] = nested
+console.log(i, j, k)
+
+//setting default values
+const [p = 1, q = 1, r = 1] = [8, 9]
+console.log(p, q, r)
