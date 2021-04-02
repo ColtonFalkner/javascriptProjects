9e50ff6e89be92a774bec421cb92d18a4dd26d79
Colton <coltonfalkner@gmail.com>
2021-04-02 10:20:37 -0400

    another day another bunch of stuff learned

--- FootballBettingApp/script.js
+++ FootballBettingApp/script.js
@@ -41,24 +41,69 @@ const game = {
   },
 }
 
-//Challenge #2
-//1. Loop over the game.scored array and print each player name to the console along with the goal number. "Goal 1: Lewandowski"
-for (const [i, player] of game.scored.entries()) {
-  console.log(`Goal ${i + 1} was scored by ${player}.`)
+const gameEvents = new Map([
+  [17, '⚽️ GOAL'],
+  [36, '🔁 Substitution'],
+  [47, '⚽️ GOAL'],
+  [61, '🔁 Substitution'],
+  [64, '🔶 Yellow card'],
+  [69, '🔴 Red card'],
+  [70, '🔁 Substitution'],
+  [72, '🔁 Substitution'],
+  [76, '⚽️ GOAL'],
+  [80, '⚽️ GOAL'],
+  [92, '🔶 Yellow card'],
+])
+
+//Challenge #3
+
+//1. Create an array "events" of the different game events that happened (no duplicates)
+console.log(gameEvents.values())
+const events = [...new Set(gameEvents.values())]
+console.log(events)
+//2.After the game has finished, it was found that the yellow card from minute 64 was unfair. So remove this event from the game vents log.
+gameEvents.delete(64)
+console.log(gameEvents)
+
+//3. Compute and log the following string to the console: "An event happened, on everage, every 9 minutes" (Keep in mind that the game has 90 minutes)
+const time = [...gameEvents.keys()].pop()
+console.log(
+  `An event happened, on average, every ${time / gameEvents.size} minutes.`
+)
+//4. Loop over 'gameEvents' and log each element to the console, marking whether it's in the first half or second half (after 45min) of the game
+for (const [min, event] of gameEvents) {
+  const half = min <= 45 ? 'First Half' : 'Second Half'
+  console.log(`${half} - Min ${min}: ${event}`)
 }
 
-//2.Use a loop to calculate the average odd (average of all the odds) and log it to the console.
-const odds = Object.values(game.odds)
-let average = 0
-for (const odd of odds) average += odd
-average /= odds.length
-console.log(average)
+/////////////////////////////////////
+/////////////////////////////////////
+/////////////////////////////////////
+/////////////////////////////////////
+/////////////////////////////////////
+/////////////////////////////////////
+/////////////////////////////////////
+/////////////////////////////////////
+/////////////////////////////////////
 
-//3. Print the 3 odds to the console, but in a nice formatted way. Get the team names directly from the object. Do not hardcode them.
-for (const [team, odd] of Object.entries(game.odds)) {
-  const teamStr = team === 'x' ? 'draw' : `Victory ${game[team]}`
-  console.log(`Odd of ${teamStr} ${odd}`)
-}
+// //Challenge #2
+// //1. Loop over the game.scored array and print each player name to the console along with the goal number. "Goal 1: Lewandowski"
+// for (const [i, player] of game.scored.entries()) {
+//   console.log(`Goal ${i + 1} was scored by ${player}.`)
+// }
+
+// //2.Use a loop to calculate the average odd (average of all the odds) and log it to the console.
+// const odds = Object.values(game.odds)
+// let average = 0
+// for (const odd of odds) average += odd
+// average /= odds.length
+// console.log(average)
+
+// //3. Print the 3 odds to the console, but in a nice formatted way. Get the team names directly from the object. Do not hardcode them.
+// for (const [team, odd] of Object.entries(game.odds)) {
+//   const teamStr = team === 'x' ? 'draw' : `Victory ${game[team]}`
+//   console.log(`Odd of ${teamStr} ${odd}`)
+// }
 
 /////////////////////////////////////
 /////////////////////////////////////
--- SetsAndMapsInPractice/script.js
+++ SetsAndMapsInPractice/script.js
@@ -48,42 +48,74 @@ const restaurant = {
   },
 }
 
-// Maps
-const rest = new Map()
-//To Add
-rest.set('name', 'Classico Italiano')
-rest.set(1, 'Firenze, Italy')
-rest.set(2, 'Lisbon, Portugal')
-console.log(rest)
-//Additions can be chained
-rest
-  .set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
-  .set('open', 11)
-  .set('close', 23)
-  .set(true, 'We are open.')
-  .set(false, 'We are closed')
-console.log(rest)
+const question = new Map([
+  ['question', 'what is the best programming language in the world? '],
+  [1, 'C'],
+  [2, 'Java'],
+  [3, 'Javascript'],
+  ['correct', 3],
+  [true, 'Correct'],
+  [false, 'Incorrect'],
+])
 
-// console.log(rest.get('name'))
-// console.log(rest.get(true))
+console.log(question)
+//Converting Map to Object
+console.log(Object.entries(openingHours))
+const hoursMap = new Map(Object.entries(openingHours))
+console.log(hoursMap)
+//Iterables
+//Quiz app
+console.log(question.get('question'))
+for (const [key, value] of question) {
+  if (typeof key === 'number') console.log(`Answer ${key}: ${value}`)
+}
 
-const time = 21
-console.log(rest.get(time > rest.get('open') && time < rest.get('close')))
-//To check what if something is in a map
-console.log(rest.has('categories'))
+// const answer = Number(prompt('Your answer.'))
+// console.log(answer)
 
-rest.delete(2)
+// console.log(question.get(question.get('correct') === answer))
 
-//You can use arrays or objects as map keys
-// rest.clear()
-const arr = [1, 2]
-rest.set(arr, 'Test')
-console.log(rest)
-//before setting the array to a variable, using the get method will result in undefined
-console.log(rest.get(arr))
+//Converting a map back to an array
+console.log([...question])
+console.log(question.entries())
+console.log([...question.keys()])
+console.log([...question.values()])
+// // Maps - Fundamentals
+// const rest = new Map()
+// //To Add
+// rest.set('name', 'Classico Italiano')
+// rest.set(1, 'Firenze, Italy')
+// rest.set(2, 'Lisbon, Portugal')
+// console.log(rest)
+// //Additions can be chained
+// rest
+//   .set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
+//   .set('open', 11)
+//   .set('close', 23)
+//   .set(true, 'We are open.')
+//   .set(false, 'We are closed')
+// console.log(rest)
 
-rest.set(document.querySelector('body'), 'body')
-console.log(rest)
+// // console.log(rest.get('name'))
+// // console.log(rest.get(true))
+
+// const time = 21
+// console.log(rest.get(time > rest.get('open') && time < rest.get('close')))
+// //To check what if something is in a map
+// console.log(rest.has('categories'))
+
+// rest.delete(2)
+
+// //You can use arrays or objects as map keys
+// // rest.clear()
+// const arr = [1, 2]
+// rest.set(arr, 'Test')
+// console.log(rest)
+// //before setting the array to a variable, using the get method will result in undefined
+// console.log(rest.get(arr))
+
+// rest.set(document.querySelector('body'), 'body')
+// console.log(rest)
 
 /////////////////////////
 /////////////////////////
