694d4b21802ddc5fc4f105f4c0c250663a0e0cb2
Colton <coltonfalkner@gmail.com>
2021-04-30 11:07:00 -0400

    challenge completed

--- ObjectChallenges/script.js
+++ ObjectChallenges/script.js
@@ -8,33 +8,76 @@
 
 //4. Create 2 'Car' objects and experimenting with calling the accelerate and brake multiple times on each one.
 
-const Car = function (make, speed) {
-  ;(this.make = make), (this.speed = speed)
+// const Car = function (make, speed) {
+//   ;(this.make = make), (this.speed = speed)
+// }
+
+// Car.prototype.accelerate = function () {
+//   this.speed += 10
+//   console.log(`${this.make} is going at ${this.speed} km/h`)
+// }
+
+// Car.prototype.brake = function () {
+//   this.speed -= 5
+//   console.log(`${this.make} is going at ${this.speed} km/h`)
+// }
+
+// const car1 = new Car('BMW', 120)
+// const car2 = new Car('Mercedes', 95)
+// console.log(car1)
+// console.log(car2)
+// car1.accelerate()
+// car1.accelerate()
+// car1.accelerate()
+// car1.brake()
+// car1.brake()
+// car1.brake()
+// car2.accelerate()
+// car2.accelerate()
+// car2.accelerate()
+// car2.brake()
+// car2.brake()
+// car2.brake()
+
+//Challenge 2
+
+//1. Re-Create Challenge #1, but this time using an ES6 Class (call it CarCl)
+
+//2. Add a getter called speedUS which returns the current speed in mph (divide by 1.6)
+
+//3. Add a setter called 'speedUS' which sets the current speed in mph (but convertse it to km/h before storing the value, by multiplying the input by 1.6)
+
+//4. Create a new car and experiment with the 'accelerate' and 'brake' methods, and with the getter and setter
+
+class CarCl {
+  constructor(make, speed) {
+    this.make = make
+    this.speed = speed
+  }
+
+  accelerate() {
+    this.speed += 10
+    console.log(`${this.make} is going at ${this.speed} km/h`)
+  }
+
+  brake() {
+    this.speed -= 5
+    console.log(`${this.make} is going at ${this.speed} km/h`)
+  }
+
+  get speedUS() {
+    return this.speed / 1.6
+  }
+
+  set speedUS(speed) {
+    this.speed = speed * 1.6
+  }
 }
 
-Car.prototype.accelerate = function () {
-  this.speed += 10
-  console.log(`${this.make} is going at ${this.speed} km/h`)
-}
-
-Car.prototype.brake = function () {
-  this.speed -= 5
-  console.log(`${this.make} is going at ${this.speed} km/h`)
-}
-
-const car1 = new Car('BMW', 120)
-const car2 = new Car('Mercedes', 95)
+const car1 = new CarCl('Ford', 120)
 console.log(car1)
-console.log(car2)
-car1.accelerate()
-car1.accelerate()
+console.log(car1.speedUS)
 car1.accelerate()
 car1.brake()
-car1.brake()
-car1.brake()
-car2.accelerate()
-car2.accelerate()
-car2.accelerate()
-car2.brake()
-car2.brake()
-car2.brake()
+car1.speedUS = 50
+console.log(car1)
--- ObjectOrientedProgramming/script.js
+++ ObjectOrientedProgramming/script.js
@@ -8,44 +8,145 @@ const Person = function (firstName, birthYear) {
   // }
 }
 
-const colton = new Person('Colton', 1991)
-console.log(colton)
+// const colton = new Person('Colton', 1991)
+// console.log(colton)
 
-const matilda = new Person('Matilda', 2012)
-const jack = new Person('Jack', 1938)
-console.log(matilda, jack)
+// const matilda = new Person('Matilda', 2012)
+// const jack = new Person('Jack', 1938)
+// console.log(matilda, jack)
 
-console.log(colton instanceof Person)
+// console.log(colton instanceof Person)
 
-//Prototypes
-console.log(Person.prototype)
+// //Prototypes
+// console.log(Person.prototype)
 
-Person.prototype.calcAge = function () {
-  console.log(2037 - this.birthYear)
+// Person.prototype.calcAge = function () {
+//   console.log(2037 - this.birthYear)
+// }
+
+// colton.calcAge()
+// console.log(colton.__proto__)
+// console.log(colton.__proto__ === Person.prototype)
+
+// Person.prototype.species = 'Homo Sapiens'
+// console.log(colton.species, matilda.species)
+
+// console.log(colton.hasOwnProperty('firstName'))
+// console.log(colton.hasOwnProperty('species'))
+
+// console.log(colton.__proto__)
+// console.log(colton.__proto__.__proto__)
+// console.log(colton.__proto__.__proto__.__proto__)
+
+// console.dir(Person.prototype.constructor)
+
+// const arr = [2, 3, 4, 6, 6, 4, 3, 6, 7]
+// console.log(arr.__proto__)
+// console.log(arr.__proto__.__proto__)
+
+// Array.prototype.unique = function () {
+//   return [...new Set(this)]
+// }
+
+// console.log(arr.unique())
+
+//ES6 Classes
+//class expression
+// const PersonCL = class {
+
+// }
+//class declaration
+class PersonCL {
+  constructor(fullName, birthYear) {
+    this.fullName = fullName
+    this.birthYear = birthYear
+  }
+  calcAge() {
+    console.log(2037 - this.birthYear)
+  }
+
+  greet() {
+    console.log(`Hey ${this.firstName}`)
+  }
+
+  get age() {
+    return 2037 - this.birthYear
+  }
+
+  set fullName(name) {
+    if (name.includes(' ')) this._fullName = name
+    else alert(`${name} is not a full name.`)
+  }
+
+  get fullName() {
+    return this._fullName
+  }
+
+  static hey() {
+    console.log('Hey there!')
+  }
 }
 
-colton.calcAge()
-console.log(colton.__proto__)
-console.log(colton.__proto__ === Person.prototype)
+const jessica = new PersonCL('Jessica Davis', 1996)
+console.log(jessica)
+// jessica.calcAge()
 
-Person.prototype.species = 'Homo Sapiens'
-console.log(colton.species, matilda.species)
+// // PersonCL.prototype.greet = function () {
+// //   console.log(`Hey ${this.firstName}`)
+// // }
 
-console.log(colton.hasOwnProperty('firstName'))
-console.log(colton.hasOwnProperty('species'))
+// jessica.greet()
 
-console.log(colton.__proto__)
-console.log(colton.__proto__.__proto__)
-console.log(colton.__proto__.__proto__.__proto__)
+const account = {
+  owner: 'jonas',
+  movements: [200, 530, 120, 300],
 
-console.dir(Person.prototype.constructor)
+  get latest() {
+    return this.movements.slice(-1).pop()
+  },
 
-const arr = [2, 3, 4, 6, 6, 4, 3, 6, 7]
-console.log(arr.__proto__)
-console.log(arr.__proto__.__proto__)
-
-Array.prototype.unique = function () {
-  return [...new Set(this)]
+  set latest(mov) {
+    this.movements.push(mov)
+  },
 }
 
-console.log(arr.unique())
+console.log(account.latest)
+
+account.latest = 50
+console.log(account.movements)
+
+console.log(jessica.age)
+
+// Person.hey = function () {
+//   console.log('Hey there!')
+// }
+// Person.hey()
+// jessica.hey()
+PersonCL.hey()
+
+const PersonProto = {
+  calcAge() {
+    console.log(2037 - this.birthYear)
+  },
+
+  init(firstName, birthYear) {
+    this.firstName = firstName
+    this.birthYear = birthYear
+  },
+}
+
+const steven = Object.create(PersonProto)
+
+console.log(steven)
+steven.name = 'Steven'
+steven.birthYear = 2002
+
+steven.calcAge()
+console.log(steven.__proto__)
+console.log(steven.__proto__ === PersonProto)
+
+const sarah = Object.create(PersonProto)
+
+console.log(sarah)
+sarah.init('Sarah', 1997)
+sarah.calcAge()
