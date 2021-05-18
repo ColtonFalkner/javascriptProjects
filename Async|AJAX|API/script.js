958b6a71336554ec4e7629b1940aacea73d7c01d
Colton <coltonfalkner@gmail.com>
2021-05-18 13:56:15 -0400

    challenges file structure created

--- Async|AJAX|API/script.js
+++ Async|AJAX|API/script.js
@@ -3,6 +3,32 @@
 const btn = document.querySelector('.btn-country')
 const countriesContainer = document.querySelector('.countries')
 
+const renderCountry = function (data, className = '') {
+  const html = `
+  <article class="country ${className}">
+  <img class="country__img" src="${data.flag}" />
+  <div class="country__data">
+    <h3 class="country__name">${data.name}</h3>
+    <h4 class="country__region">${data.region}</h4>
+    <p class="country__row"><span>👫</span>${(
+      +data.population / 1000000
+    ).toFixed(1)} people</p>
+    <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+    <p class="country__row"><span>💰</span>${data.currencies[0].code}</p>
+  </div>
+  </article>
+  `
+
+  countriesContainer.insertAdjacentHTML('beforeend', html)
+  // countriesContainer.style.opacity = 1
+}
+
+const renderError = function (msg) {
+  countriesContainer.insertAdjacentText('beforeend', msg)
+
+  // countriesContainer.style.opacity = 1
+}
+
 ///////////////////////////////////////
 // function getCountryData(country) {
 //   const request = new XMLHttpRequest()
@@ -37,25 +63,6 @@ const countriesContainer = document.querySelector('.countries')
 // }
 // getCountryData('usa')
 /////////////////////////////////////////////////////////////
-const renderCountry = function (data, className = '') {
-  const html = `
-  <article class="country ${className}">
-  <img class="country__img" src="${data.flag}" />
-  <div class="country__data">
-    <h3 class="country__name">${data.name}</h3>
-    <h4 class="country__region">${data.region}</h4>
-    <p class="country__row"><span>👫</span>${(
-      +data.population / 1000000
-    ).toFixed(1)} people</p>
-    <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
-    <p class="country__row"><span>💰</span>${data.currencies[0].code}</p>
-  </div>
-  </article>
-  `
-
-  countriesContainer.insertAdjacentHTML('beforeend', html)
-  countriesContainer.style.opacity = 1
-}
 
 // function getCountryAndNeighbor(country) {
 //   //AJAX call country 1
@@ -103,23 +110,72 @@ const request = fetch('https:restcountries.eu/rest/v2/name/portugal')
 //     })
 // }
 
+// const getCountryData = function (country) {
+//   fetch(`https:restcountries.eu/rest/v2/name/${country}`)
+//     .then((response) => {
+//       console.log(response)
+
+//       if (!response.ok)
+//         throw new Error(`Country not found (${response.status})`)
+
+//       return response.json()
+//     })
+//     .then((data) => {
+//       renderCountry(data[0])
+//       const neighbour = data[0].borders[0]
+
+//       if (!neighbour) return
+
+//       return fetch(`https://restcountries.eu/rest/v2/alpha/${neighbour}`)
+//     })
+//     .then((response) => response.json())
+//     .then((data) => renderCountry(data, 'neighbour'))
+//     .catch((err) => {
+//       console.error(`${err} Done Fucked Up`)
+//       renderError(`Something Went Wrong || ${err.message}`)
+//     })
+//     .finally(() => {
+//       countriesContainer.style.opacity = 1
+//     })
+// }
+
+const getJSON = function (url, errorMsg = 'Something Went Wrong') {
+  return fetch(url).then((response) => {
+    if (!response.ok) throw new Error(`${errorMsg} (${response.status})`)
+
+    return response.json()
+  })
+}
+
 const getCountryData = function (country) {
-  fetch(`https:restcountries.eu/rest/v2/name/${country}`)
-    .then((response) => response.json())
+  getJSON(
+    `https://restcountries.eu/rest/v2/name/${country}`,
+    'Country Not Found'
+  )
     .then((data) => {
       renderCountry(data[0])
       const neighbour = data[0].borders[0]
 
-      if (!neighbour) return
+      if (!neighbour) throw new Error('No Neighbor Found!!')
 
-      return fetch(`https://restcountries.eu/rest/v2/alpha/${neighbour}`)
+      return getJSON(
+        `https://restcountries.eu/rest/v2/alpha/${neighbour}`,
+        'Neighbor Not Found'
+      )
     })
-    .then((response) => response.json())
     .then((data) => renderCountry(data, 'neighbour'))
-    .catch((err) => console.error(`${err} Done Fucked Up`))
+    .catch((err) => {
+      console.error(`${err}`)
+      renderError(`Something Went Wrong || ${err.message}`)
+    })
+    .finally(() => {
+      countriesContainer.style.opacity = 1
+    })
 }
 
 btn.addEventListener('click', function () {
-  getCountryData('usa')
+  getCountryData('australia')
 })
+
 // getCountryData('usa')
+// getCountryData('ffff')
